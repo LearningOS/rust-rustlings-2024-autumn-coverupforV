716ac8db201f1211770bd7f0bf3e3975f729bce4
coverupforV <pcw760404931@163.com>
2024-10-18 23:40:08 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -8,26 +8,17 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    
-    // 使用 if let 来处理 Option
-    if my_option.is_none() {
-        println!("my_option is None!");
-    }
 
-    // 修复数组定义，使用逗号分隔
-    let my_arr = &[
-        -1, -2, -3, // 添加逗号
-        -4, -5, -6  // 添加逗号
-    ];
+    let my_arr = &[-1, -2, -3, -4, -5, -6];
     println!("My array! Here it is: {:?}", my_arr);
 
-    // 正确创建一个空 Vec
-    let my_empty_vec: Vec<i32> = Vec::new();
+    let mut my_empty_vec = vec![1, 2, 3, 4, 5];
+    my_empty_vec.clear();
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // 使用 std::mem::swap 来交换这两个值
+    // Let's swap these two!
     std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
